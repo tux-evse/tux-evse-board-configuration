061b7c502dd38a90749f40fe92c88b1cdcdb1d92
Valentin Geffroy <valentin.geffroy@iot.bzh>
2023-11-30 16:42:06 +0100

    firewallD rules ajustments
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- firewall/config-firewall.sh
+++ firewall/config-firewall.sh
@@ -5,3 +5,7 @@
 firewall-cmd --zone=work --add-interface=eth0 --permanent
 firewall-cmd --zone=external --add-interface=wlan0 --permanent
 firewall-cmd --zone=public --add-interface=usb0 --permanent
+
+# the policies have been configured during the RPM installation
+# reload to apply the rules
+firewall-cmd --reload
