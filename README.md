4dbcba30225d20aa3c9ab02787314087f232f96a
Valentin Geffroy <valentin.geffroy@iot.bzh>
2023-11-30 15:05:14 +0100

    network: Improvements on the wifi hotspot creation
    unit systemD after Network Manager (check packaging)
    
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
-# Tux-evse-board-configuration
+# tux-evse-board-configuration
 
 ## At the first boot
 
 At the first boot you need to configure some point on the board.
 
-### uboot
+### U-Boot
 
 [README.md](./uboot/README.md)
 
-### config the repositories
+### redpesk repositories
 
 [README.md](./repositories_config/README.md)
 
-## System config
+## System configuration
 
-### The firewall
+### Network
 
-### the network
+- Ethernet: auto configured as eth0
+- LTE: auto configured as usb0
+- Bluetooth: not configured (coming soon)
+- Wifi Configuration: manually configured as wlan0
+
+### Firewall
+
+Thanks to some Linux settings and firewallD rules, this is possible to allow traffic between differents interfaces as needed for the project.
--- network/config-network.sh
+++ network/config-network.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# ethernet : auto configured
-# lte : auto configured
-# BLE : no configured yet
-# wifi : configured bellow
-nmcli device wifi hotspot ifname wlan0 con-name Valeo_Demo_Charger ssid Valeo_Charger_Demo password "valeocharger"
-nmcli connection up Valeo_Demo_Charger
+# Ethernet : auto configured as eth0
+# LTE : auto configured as usb0
+# Bluetooth : not configured yet
+
+# wifi configuration
+interface="wlan0"
+connection_name="tuxevse_hotspot"
+ssid="tuxevse_hotspot"
+password="valeocharger" # visible password for the demo
+
+# check if the interface is present
+if [ -e "/sys/class/net/$interface" ]; then
+    # hotspot wifi creation
+    nmcli device wifi hotspot ifname "$interface" con-name "$connection_name" ssid "$ssid" password "$password"
+    nmcli connection up "$connection_name"
+else
+    # system error
+    echo "The $interface interface doesn't exist or is not configured." >&2
+    exit 1
+fi
