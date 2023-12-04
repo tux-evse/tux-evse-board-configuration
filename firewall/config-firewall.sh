0a6aa6b137e4c611722d68236fbbd3295ef6d988
Valentin Geffroy <valentin.geffroy@iot.bzh>
2023-12-04 17:45:26 +0100

    firewall rules for micro-servcies ports
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- firewall/config-firewall.sh
+++ firewall/config-firewall.sh
@@ -7,5 +7,10 @@ firewall-cmd --zone=external --add-interface=wlan0 --permanent
 firewall-cmd --zone=public --add-interface=usb0 --permanent
 
 # the policies have been configured during the RPM installation
+firewall-cmd --permanent --zone=public --add-port=80/tcp
+firewall-cmd --permanent --zone=public --add-port=8080/tcp
+firewall-cmd --permanent --zone=public --add-port=8081/tcp
+firewall-cmd --permanent --zone=public --add-port=1234/tcp
+
 # reload to apply the rules
 firewall-cmd --reload
