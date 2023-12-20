ceaef226a2459c3d37b6c74eda630d6696c9da76
Valentin Geffroy <valentin.geffroy@iot.bzh>
2023-12-20 12:17:28 +0100

    wiif hotspot launch script improvement
    JSON file
    
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- hotspot_wifi/config-hotspot.sh
+++ hotspot_wifi/config-hotspot.sh
@@ -2,6 +2,6 @@
 
 # move to projet base to get relative path to logo image
 export DIR=`find / -name conf-captive-portal.json`
-cd $DIR
+cd -P `dirname $DIR`
 
 sudo PWD=$PWD afb-binder -vvv --tracereq=all --config=conf-captive-portal.json
