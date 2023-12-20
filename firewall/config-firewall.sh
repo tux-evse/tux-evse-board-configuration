2843f4f35202a3165f69b30af7303b266df92742
Valentin Geffroy <valentin.geffroy@iot.bzh>
2023-12-20 16:00:28 +0100

    another port added for the firewall
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- firewall/config-firewall.sh
+++ firewall/config-firewall.sh
@@ -6,12 +6,12 @@ firewall-cmd --zone=external --add-interface=wlan0 --permanent
 firewall-cmd --zone=public --add-interface=usb0 --permanent
 
 # authorized ports for the 4G modem
-firewall-cmd --permanent --zone=public --add-port=80/tcp --add-port=8080/tcp --add-port=8081/tcp --add-port=1234/tcp --add-port=8082/tcp
+firewall-cmd --permanent --zone=public --add-port=80/tcp --add-port=8080/tcp --add-port=8081/tcp --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=8082/tcp
 # authorized ports for the eth0
-firewall-cmd --permanent --zone=work --add-port=1234/tcp --add-port=22/tcp --add-port=80/tcp --add-port=443/tcp --add-port=8080/tcp --add-port=8082/tcp
+firewall-cmd --permanent --zone=work --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=22/tcp --add-port=80/tcp --add-port=443/tcp --add-port=8080/tcp --add-port=8082/tcp
 firewall-cmd --permanent --zone=work --add-service=cockpit
 # authorized ports for the wlan0
-firewall-cmd --permanent --zone=external --add-port=8080/tcp --add-port=8081/tcp --add-port=1234/tcp --add-port=8082/tcp
+firewall-cmd --permanent --zone=external --add-port=8080/tcp --add-port=8081/tcp --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=8082/tcp
 
 # reload to apply the rules
 firewall-cmd --reload
