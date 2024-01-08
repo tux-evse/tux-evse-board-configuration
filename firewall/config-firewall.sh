3865b15587d760fcc04e68d649e8c8edaf616e52
Valentin Geffroy <valentin.geffroy@iot.bzh>
2024-01-08 14:58:57 +0100

    firewallD: Added new ports and protocols for wlan0
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- firewall/config-firewall.sh
+++ firewall/config-firewall.sh
@@ -7,11 +7,12 @@ firewall-cmd --zone=public --add-interface=usb0 --permanent
 
 # authorized ports for the 4G modem
 firewall-cmd --permanent --zone=public --add-port=80/tcp --add-port=8080/tcp --add-port=8081/tcp --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=8082/tcp
+firewall-cmd --permanent --zone=public --add-protocol=icmp
 # authorized ports for the eth0
 firewall-cmd --permanent --zone=work --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=22/tcp --add-port=80/tcp --add-port=443/tcp --add-port=8080/tcp --add-port=8082/tcp
 firewall-cmd --permanent --zone=work --add-service=cockpit
 # authorized ports for the wlan0
-firewall-cmd --permanent --zone=external --add-port=8080/tcp --add-port=8081/tcp --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=8082/tcp
-
+firewall-cmd --permanent --zone=external --add-port=80/tcp --add-port=443/tcp --add-port=8080/tcp --add-port=8081/tcp --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=8082/tcp
+firewall-cmd --permanent --zone=external  --add-service=http --add-service=https --add-service=dhcp
 # reload to apply the rules
 firewall-cmd --reload
