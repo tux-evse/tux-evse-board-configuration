18cf1da77692f6c7bda7d5a8047c6dae63ee57ab
Valentin Geffroy <valentin.geffroy@iot.bzh>
2024-01-15 14:51:32 +0100

    captive portal https added
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- hotspot_wifi/config-hotspot.sh
+++ hotspot_wifi/config-hotspot.sh
@@ -4,14 +4,18 @@
 export DIR=`find / -name conf-captive-portal.json`
 cd -P `dirname $DIR`
 
-
-
+# symbolics for the different requests
 if [ ! -e html/generate_204 ]; then
     ln -s html/index.html html/canonical.html # linux pop-up
     ln -s html/assets/tux-evsex250.png html/favicon.ico # icon
     ln -s html/index.html html/chat # android
     ln -s html/index.html html/success.txt # linux pop-up
     ln -s html/index.html html/generate_204 # android
+fi
+
+# redirection name for the https
+if grep -q "tuxevse.iot.bzh" "/etc/hosts"; then
+    sed -i '/^127.0.0.1/s/$/ tuxevse.iot.bzh/' /etc/hosts
 fi
 
 sudo PWD=$PWD afb-binder -vvv --tracereq=all --config=conf-captive-portal.json
