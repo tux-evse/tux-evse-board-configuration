0aa9ce1277d42f599178813a1666aabfde38cb5f
Valentin Geffroy <valentin.geffroy@iot.bzh>
2024-01-19 17:13:32 +0100

    better firewalld conf with ranges
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- firewall/config-firewall.sh
+++ firewall/config-firewall.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 # script launched at every boot to config the firewall
-firewall-cmd --zone=work --add-interface=eth0 --permanent
+firewall-cmd --zone=work --add-interface=eth0 --add-interface=eth1 --permanent
 firewall-cmd --zone=external --add-interface=wlan0 --permanent
 firewall-cmd --zone=public --add-interface=usb0 --permanent
 
 # authorized ports for the 4G modem
-firewall-cmd --permanent --zone=public --add-port=80/tcp --add-port=8080/tcp --add-port=8081/tcp --add-port=1230/tcp --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=1238/tcp --add-port=8082/tcp
+firewall-cmd --permanent --zone=public --add-port=80/tcp # os settings
+firewall-cmd --permanent --zone=public --add-port=8080-8082/tcp --add-port=1200-1299/tcp 
 firewall-cmd --permanent --zone=public --add-protocol=icmp
+
 # authorized ports for the eth0
-firewall-cmd --permanent --zone=work --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=1238/tcp --add-port=1230/tcp --add-port=22/tcp --add-port=80/tcp --add-port=443/tcp --add-port=8080/tcp --add-port=8082/tcp
+firewall-cmd --permanent --zone=work --add-port=22/tcp --add-port=80/tcp --add-port=443/tcp # os settings
+firewall-cmd --permanent --add-port=8080-8082/tcp --add-port=1200-1299/tcp # for bindings
 firewall-cmd --permanent --zone=work --add-service=cockpit
+
 # authorized ports for the wlan0
-firewall-cmd --permanent --zone=external --add-port=80/tcp --add-port=443/tcp --add-port=8080/tcp --add-port=1230/tcp --add-port=8081/tcp --add-port=1234/tcp --add-port=1235/tcp --add-port=1236/tcp --add-port=1238/tcp --add-port=8082/tcp
+firewall-cmd --permanent --zone=external --add-port=80/tcp --add-port=443/tcp # os settings
+firewall-cmd --permanent --zone=external --add-port=8080-8082/tcp --add-port=1200-1299/tcp # for bindings
 firewall-cmd --permanent --zone=external  --add-service=http --add-service=https --add-service=dhcp
+
 # reload to apply the rules
 firewall-cmd --reload
