dd101552858890f5feddad4f89e753729df50b27
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-02-07 14:48:12 +0100

    change umask for socket access
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- cynagora/cynagora-debug-configuration.sh
+++ cynagora/cynagora-debug-configuration.sh
@@ -2,3 +2,10 @@
 echo "Add HELLO to cynagora db"
 
 cynagora-admin set '' 'HELLO' '' '*' yes
+
+# If user "root" uses script in debug mode and so create socket in /tmp/api,
+# user "rp-owner" must be able to rw the socket
+mkdir -p /tmp/api
+chgr users /tmp/api
+chmod g+s /tmp/api
+sed -i -E "s/    umask 022/    umask 011/g" /etc/profile
