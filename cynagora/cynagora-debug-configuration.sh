cadfe80021d1dab4551e0fdeecf4f0438e54c085
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-02-13 09:51:02 +0100

    update cynagora-debug-configuration.sh script
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- cynagora/cynagora-debug-configuration.sh
+++ cynagora/cynagora-debug-configuration.sh
@@ -6,7 +6,9 @@ cynagora-admin set '' 'HELLO' '' '*' yes
 # If user "root" uses script in debug mode and so create socket in /tmp/api,
 # user "rp-owner" must be able to rw the socket
 mkdir -p /tmp/api
+rm -fr /tmp/api/*
 chgrp users /tmp/api
-chmod g+s /tmp/api
+chmod g+srw /tmp/api
+chsmack -a System /tmp/api
 #change de umask for root session
 sed -i -E "s/    umask 022/    umask 011/g" /etc/profile
