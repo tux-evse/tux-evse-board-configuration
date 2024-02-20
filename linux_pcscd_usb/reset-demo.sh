d15b8864e9fc4682f00cc6838765f51e07b8b4fb
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-02-20 14:19:22 +0100

    Add init smack rules
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- linux_pcscd_usb/reset-demo.sh
+++ linux_pcscd_usb/reset-demo.sh
@@ -11,6 +11,16 @@ su - rp-owner afm-util kill display-manager
 echo "Kill tux-evse-webapp"
 su - rp-owner afm-util kill tux-evse-webapp
 
+#----------------------------------------
+chsmack -a "App:auth-manager:Conf" /usr/redpesk/evse-auth-manager-binder/etc/*
+chsmack -a "App:charging-manager:Conf" /usr/redpesk/evse-charging-manager-binder/etc/*
+chsmack -a "App:display-manager:Conf" /usr/redpesk/evse-display-manager-binder/etc/*
+chsmack -a "App:energy-manager:Conf" /usr/redpesk/evse-energy-manager-binder/etc/*
+
+chsmack -a "App:tux-evse-webapp:Conf" /usr/redpesk/tux-evse-webapp/etc/*
+
+#----------------------------------------
+
 echo "Restart display-manager"
 su - rp-owner afm-util run display-manager
 
