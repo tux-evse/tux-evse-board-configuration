65493b6af402cb8e2c5b9bc8aee1cce45a04e1e6
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-02-22 17:41:13 +0100

    Fix reset-demo
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- linux_pcscd_usb/reset-demo.sh
+++ linux_pcscd_usb/reset-demo.sh
@@ -12,6 +12,9 @@ echo "Kill tux-evse-webapp"
 su - rp-owner afm-util kill tux-evse-webapp
 
 #----------------------------------------
+pkill -9 -U 1001
+
+#----------------------------------------
 chsmack -a "App:auth-manager:Conf" /usr/redpesk/evse-auth-manager-binder/etc/*
 chsmack -a "App:charging-manager:Conf" /usr/redpesk/evse-charging-manager-binder/etc/*
 chsmack -a "App:display-manager:Conf" /usr/redpesk/evse-display-manager-binder/etc/*
@@ -20,6 +23,15 @@ chsmack -a "App:energy-manager:Conf" /usr/redpesk/evse-energy-manager-binder/etc
 chsmack -a "App:tux-evse-webapp:Conf" /usr/redpesk/tux-evse-webapp/etc/*
 
 #----------------------------------------
+
+echo "Restart energy-manager"
+su - rp-owner afm-util run energy-manager
+
+echo "Restart auth-manager"
+su - rp-owner afm-util run auth-manager
+
+echo "Restart charging-manager"
+su - rp-owner afm-util run charging-manager
 
 echo "Restart display-manager"
 su - rp-owner afm-util run display-manager
