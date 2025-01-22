b9b72df7d850cb7576932b190f10f33989c3e522
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-01-22 18:00:26 +0100

    Fix typo
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- firewall/config-firewall.sh
+++ firewall/config-firewall.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# add ports to a zone
+# Add ports to a zone
 add_ports() {
     echo "Adding ports to $1 zone: $2"
     firewall-cmd --permanent --zone="$1" --add-port="$2" &>/dev/null
 }
 
-# add services to a zone
+# Add services to a zone
 add_services() {
     echo "Adding services to $1 zone: $2"
     firewall-cmd --permanent --zone="$1" --add-service="$2" &>/dev/null
 }
 
-# add interfaces to firewall zones
+# Add interfaces to firewall zones
 add_interfaces() {
     zone="$1"
     shift
@@ -32,22 +32,22 @@ add_interfaces "external" "wlan0"
 add_interfaces "public" "usb0"
 add_interfaces "trusted" "eth2"
 
-# wireguard VPN interface
+# Wireguard VPN interface
 wg_interface=$(nmcli -g name con | grep wg)
 if [ -n "$wg_interface" ] && ! firewall-cmd --zone=work --query-interface="$wg_interface"; then
     add_interfaces "work" "$wg_interface"
 fi
 
 echo "-- firewallD 4G modem configuration --"
-add_ports "public" "80/tcp,8080-8082/tcp,1200-1299/tcp"
+add_ports "public" "80/tcp, 8080-8082/tcp, 1200-1299/tcp"
 
 echo "-- firewallD eth0 configuration --"
-add_ports "work" "22/tcp,80/tcp,443/tcp,8080-8082/tcp,1200-1299/tcp"
+add_ports "work" "22/tcp, 80/tcp, 443/tcp, 8080-8082/tcp, 1200-1299/tcp"
 add_services "work" "cockpit"
 
 echo "-- firewallD wlan0 configuration --"
-add_ports "external" "80/tcp,443/tcp,8080-8082/tcp,1200-1299/tcp"
-add_services "external" "http,https,dhcp"
+add_ports "external" "80/tcp, 443/tcp, 8080-8082/tcp, 1200-1299/tcp"
+add_services "external" "http, https, dhcp"
 
 echo "-- firewallD reload to apply changes --"
 firewall-cmd --reload
