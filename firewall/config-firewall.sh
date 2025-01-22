44dd4ed181fe1643d9c44a26e5df52d743553853
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-01-22 17:51:57 +0100

    firewallD: Optimize script
    Faster with less firewall-cmd call
    
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- firewall/config-firewall.sh
+++ firewall/config-firewall.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-# add a port to a zone
-add_port() {
-    echo "Adding $2 port to $1 zone..."
+# add ports to a zone
+add_ports() {
+    echo "Adding ports to $1 zone: $2"
     firewall-cmd --permanent --zone="$1" --add-port="$2" &>/dev/null
 }
 
-# add a service to a zone if not already added
-add_service() {
-    echo "Adding $2 service to $1 zone..."
+# add services to a zone
+add_services() {
+    echo "Adding services to $1 zone: $2"
     firewall-cmd --permanent --zone="$1" --add-service="$2" &>/dev/null
 }
 
-# add interfaces to firewall zones even if they don't exist
+# add interfaces to firewall zones
 add_interfaces() {
-    if [[ $(firewall-cmd --zone="$1" --query-interface="$2") == "no" ]]; then
-        echo "Adding $2 interface to $1 zone..."
-        firewall-cmd --permanent --zone="$1" --add-interface="$2"
-    else
-        echo "$2 interface already in $1 zone!"
-    fi
+    zone="$1"
+    shift
+    for interface in "$@"; do
+        if [[ $(firewall-cmd --zone="$zone" --query-interface="$interface") == "no" ]]; then
+            echo "Adding $interface interface to $zone zone..."
+            firewall-cmd --permanent --zone="$zone" --add-interface="$interface" &>/dev/null
+        else
+            echo "$interface interface already in $zone zone!"
+        fi
+    done
 }
 
 echo "-- firewallD interfaces configuration --"
-add_interfaces "work" "eth0"
-add_interfaces "work" "eth1"
+add_interfaces "work" "eth0" "eth1"
 add_interfaces "external" "wlan0"
 add_interfaces "public" "usb0"
 add_interfaces "trusted" "eth2"
@@ -36,30 +39,15 @@ if [ -n "$wg_interface" ] && ! firewall-cmd --zone=work --query-interface="$wg_i
 fi
 
 echo "-- firewallD 4G modem configuration --"
-# add ports for the 4G modem
-add_port "public" "80/tcp"
-add_port "public" "8080-8082/tcp"
-add_port "public" "1200-1299/tcp"
+add_ports "public" "80/tcp,8080-8082/tcp,1200-1299/tcp"
 
 echo "-- firewallD eth0 configuration --"
-# add ports/services for eth0
-add_port "work" "22/tcp"
-add_port "work" "80/tcp"
-add_port "work" "443/tcp"
-add_port "work" "8080-8082/tcp"
-add_port "work" "1200-1299/tcp"
-add_service "work" "cockpit"
+add_ports "work" "22/tcp,80/tcp,443/tcp,8080-8082/tcp,1200-1299/tcp"
+add_services "work" "cockpit"
 
 echo "-- firewallD wlan0 configuration --"
-# add ports/services for wlan0
-add_port "external" "80/tcp"
-add_port "external" "443/tcp"
-add_port "external" "8080-8082/tcp"
-add_port "external" "1200-1299/tcp"
-add_service "external" "http"
-add_service "external" "https"
-add_service "external" "dhcp"
+add_ports "external" "80/tcp,443/tcp,8080-8082/tcp,1200-1299/tcp"
+add_services "external" "http,https,dhcp"
 
 echo "-- firewallD reload to apply changes --"
-# reload to apply the rules
 firewall-cmd --reload
